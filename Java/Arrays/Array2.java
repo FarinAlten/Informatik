1852eb95a32835d634e06b57de5ee253512a3a9d
Farin Altenhöner <Farin.alten@gmail.com>
2025-12-08 14:21:27 +0100

    4,2/4,3

--- Java/Arrays/Array2.java
+++ Java/Arrays/Array2.java
@@ -1,11 +1,29 @@
 public class Array2 {
+
+    int[] zahlen = {1, 2, 3, 4, 5};
+
     public static void main(String[] args) {
+        Array2 arrayObjekt = new Array2();
 
-        int[] zahlen = {1, 2, 3, 4, 5};
-
-        for (int i = 0; i < zahlen.length; i++) {
-            
-            System.out.println(zahlen[i]);
+        int summe = 0;
+        for (int i = 0; i < arrayObjekt.zahlen.length; i++) {
+            System.out.println(arrayObjekt.zahlen[i]);
+            summe += arrayObjekt.zahlen[i];
         }
+
+        System.out.println("Summe aller Werte: " + summe);
+
+        int groessterWert = arrayObjekt.max();
+        System.out.println("Maximalster Wert: " + groessterWert);
+    }
+
+    public int max() {
+        int maxWert = zahlen[0];
+        for (int i = 1; i < zahlen.length; i++) {
+            if (zahlen[i] > maxWert) {
+                maxWert = zahlen[i];
+            }
+        }
+        return maxWert;
     }
 }
