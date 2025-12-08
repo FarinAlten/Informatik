064a9f2711f01a906322fbb9db62c0605ba72032
Farin Altenhöner <Farin.alten@gmail.com>
2025-12-08 14:06:48 +0100

    bugs

--- Java/Arrays/Array2.java
+++ Java/Arrays/Array2.java
@@ -1,3 +1,11 @@
 public class Array2 {
-    
+    public static void main(String[] args) {
+
+        int[] zahlen = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < zahlen.length; i++) {
+            
+            System.out.println(zahlen[i]);
+        }
+    }
 }
