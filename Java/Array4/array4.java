088f3d5a44cf2dc6a0bef40c12d2bd886e4f9178
Farin Altenhöner <Farin.alten@gmail.com>
2025-12-11 08:03:04 +0100

    11.12.2025 Aufgabe 4.4

---
+++ Java/Array4/array4.java
@@ -0,0 +1,16 @@
+public class array3 {
+public static void main (String[] args) {
+
+int [] zahlen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+for (int i = 0; i < zahlen.length; i++) {
+zahlen [i] = zahlen [i] * 2;
+
+System.out.println ("Verdoppelte Werte:");
+}
+for (int i = 0; i < zahlen.length; i++) {
+System.out.print (zahlen [i] + " ");
+
+            }
+        }
+    }
